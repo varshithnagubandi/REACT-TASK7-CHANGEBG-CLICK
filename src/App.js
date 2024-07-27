c827704feea8c8e276bf1c98435b1ff2d1c327fc
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-07-27 19:46:44 +0530

    changebg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [background, setBackground] = useState("black");
+  const handleClick = () => {
+    const newColor = background === "black" ? "red" : "black";
+    setBackground(newColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        background,
+        height: "500px",
+        width: "500px",
+      }}
+    >
+      <div className="button">
+        <button onClick={handleClick}>CLICK ME</button>
+      </div>
     </div>
   );
 }
